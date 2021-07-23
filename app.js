5d3fa262199c050c062d51d2932dd137f9235c5a
Satya Prakash Nandy <satyaprakashnandy@gmail.com>
2021-07-23 21:54:39 +0530

    Initial Setup

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// Set global settings.
+app.set('view engine','pug');
+// Note : Default configuration for views is - views in root directory.
+app.set('views','views'); 
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
