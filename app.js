3f69bf7d0f56253a4ba74ad5dc59092fe24c5cd5
Satya Prakash Nandy <satyaprakashnandy@gmail.com>
2021-07-24 15:34:23 +0530

    Used Pug Template Engine

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 // Set global settings.
-app.set('view engine','pug');
+app.set("view engine", "pug");
 // Note : Default configuration for views is - views in root directory.
-app.set('views','views'); 
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes.router);
+app.use("/admin", adminRoutes.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,21 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
-const admin = require('./admin');
+const admin = require("./admin");
 
-
-router.get('/', (req, res, next) => {
-  console.log(admin.products)
-  res.render('shop',{prods:admin.products,docTitle:'shop'})
+router.get("/", (req, res, next) => {
+  console.log(admin.products);
+  res.render("shop", {
+    prods: admin.products,
+    docTitle: "shop",
+    path: "/",
+    pageTitle: "My Shop",
+  });
 });
 
 module.exports = router;
