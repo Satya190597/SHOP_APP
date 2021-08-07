5778d992c80363bc84c5b3bc544b4f7250fc4be3
Satya Prakash Nandy <satyaprakashnandy@gmail.com>
2021-08-07 13:57:16 +0530

    Show Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,8 +7,6 @@ const path = require("path");
 const Product = require("../models/product");
 const Cart = require("../models/cart");
 
-
-
 const products = [];
 
 module.exports.getProducts = (request, response) => {
@@ -45,20 +43,38 @@ module.exports.getIndex = (request, response) => {
 };
 
 module.exports.getCart = (request, response) => {
-  response.render("shop/cart", { path: "/cart", pageTitle: "Your Cart" });
+  Cart.getCarts((cartItems) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cartItems.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({
+            data: product,
+            quantity: cartProductData.quantity,
+          });
+        }
+      }
+      response.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        cartProducts: cartProducts,
+      });
+    });
+  });
 };
 
-module.exports.postCart = (request,response) => {
-
+module.exports.postCart = (request, response) => {
   const productId = request.body.productId;
 
-  Product.findById(productId,(product) => {
-    Cart.addProduct(product.id,product.price)
-  })
+  Product.findById(productId, (product) => {
+    Cart.addProduct(product.id, product.price);
+  });
 
   return response.redirect("/cart");
-
-}
+};
 
 module.exports.getCheckout = (request, response) => {
   response.render("shop/checkout", {
--- models/cart.js
+++ models/cart.js
@@ -8,7 +8,7 @@ module.exports = class Cart {
     // Fetch previous Cart.
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
-      if (!err && fileContent.length>0) {
+      if (!err && fileContent.length > 0) {
         cart = JSON.parse(fileContent);
       }
       const existingProduct = cart.products.find(
@@ -27,29 +27,44 @@ module.exports = class Cart {
         updatedProduct = { id: id, quantity: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
-      cart.totalPrice =  parseFloat(cart.totalPrice) + parseFloat(productPrice);
-      fs.writeFile(p,JSON.stringify(cart),error => {
-        console.log(error)
-        })
+      cart.totalPrice = parseFloat(cart.totalPrice) + parseFloat(productPrice);
+      fs.writeFile(p, JSON.stringify(cart), (error) => {
+        console.log(error);
+      });
     });
     // Analyze the cart => Find existing product.
     // Add new product or increase quantity.
   }
-  static deleteProduct(id,productPrice) {
+  static deleteProduct(id, productPrice) {
     let cart = { products: [], totalPrice: 0 };
-    fs.readFile(p,(err,fileContent) => {
-      if(err && fileContent.length>0) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err && fileContent.length > 0) {
         return;
       }
       let cart = JSON.parse(fileContent);
-      const updatedCart = {...cart}
+      const updatedCart = { ...cart };
       // Find the quantity of the product.
-      const productIndex = updatedCart.products.findIndex(product => product.id === id);
-      updatedCart.totalPrice = updatedCart.totalPrice - updatedCart.products[productIndex].quantity * productPrice;
-      updatedCart.products = updatedCart.products.filter(product => product.id !== id);
-      fs.writeFile(p,JSON.stringify(updatedCart),error => {
-        console.log('Product Deleted From The Cart ....')
-      })
-    })
+      const productIndex = updatedCart.products.findIndex(
+        (product) => product.id === id
+      );
+      updatedCart.totalPrice =
+        updatedCart.totalPrice -
+        updatedCart.products[productIndex].quantity * productPrice;
+      updatedCart.products = updatedCart.products.filter(
+        (product) => product.id !== id
+      );
+      fs.writeFile(p, JSON.stringify(updatedCart), (error) => {
+        console.log("Product Deleted From The Cart ....");
+      });
+    });
+  }
+
+  static getCarts(cb) {
+    fs.readFile(p, (error, fileContent) => {
+      if (error) {
+        cb(null);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 };
